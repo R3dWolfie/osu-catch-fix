90d15a49923ac06e1979bc62fcaf6c54d26e530d
R3D <arui939@gmail.com>
2026-09-25 12:34:22 +0000

    Run catch gameplay at full update rate and draw every update

--- osu.Game.Rulesets.Catch/UI/DrawableCatchRuleset.cs
+++ osu.Game.Rulesets.Catch/UI/DrawableCatchRuleset.cs
@@ -1,10 +1,12 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using osu.Framework.Allocation;
 using osu.Framework.Input;
+using osu.Framework.Platform;
 using osu.Game.Beatmaps;
 using osu.Game.Configuration;
 using osu.Game.Input.Handlers;
@@ -33,12 +35,41 @@ namespace osu.Game.Rulesets.Catch.UI
             VisualisationMethod = ScrollVisualisationMethod.Constant;
         }
 
+        [Resolved]
+        private GameHost host { get; set; } = null!;
+
+        private double? previousDrawHz;
+        private double? previousUpdateHz;
+
         [BackgroundDependencyLoader]
         private void load()
         {
             // With relax mod, input maps directly to x position and left/right buttons are not used.
             if (!Mods.Any(m => m is ModRelax))
                 KeyBindingInputManager.Add(new CatchTouchInputMapper());
+        }
+
+        protected override void LoadComplete()
+        {
+            base.LoadComplete();
+
+            // catch is played by watching the catcher, so run update and draw at full rate during gameplay to keep input to screen delay low.
+            // the previous rates are restored on dispose.
+            previousDrawHz = host.MaximumDrawHz;
+            previousUpdateHz = host.MaximumUpdateHz;
+
+            host.MaximumUpdateHz = Math.Max(host.MaximumUpdateHz, 1000);
+            host.MaximumDrawHz = Math.Max(host.MaximumDrawHz, host.MaximumUpdateHz);
+        }
+
+        protected override void Dispose(bool isDisposing)
+        {
+            if (previousDrawHz != null)
+                host.MaximumDrawHz = previousDrawHz.Value;
+            if (previousUpdateHz != null)
+                host.MaximumUpdateHz = previousUpdateHz.Value;
+
+            base.Dispose(isDisposing);
         }
 
         protected double GetTimeRange(float approachRate) => IBeatmapDifficultyInfo.DifficultyRange(approachRate, 1800, 1200, 450);
